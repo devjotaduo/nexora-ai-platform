devjotaduo/nexora-ai-platform

commit 4a57717bab04613b50b0ae772df2ee08d65d0ab0

sync: sanitize remaining CJ_AIOPS env var references
Replace uppercase CJ_AIOPS_* environment variable names with NEXORA_*
across compose files, Python config, startup scripts, and docs.

deploy/entrypoint.sh
@@ -43,16 +43,6 @@ else
   echo "Config found in ${QWENPAW_WORKING_DIR}, skipping initialization."
 fi
 
-# Run Alembic migrations if NEXORA_DB_URL is set.
-if [ -n "${NEXORA_DB_URL:-}" ]; then
-    echo "Running database migrations..."
-    if alembic upgrade head; then
-        echo "Database migrations complete."
-    else
-        echo "WARNING: Database migration failed. Tables may be created on first access." >&2
-    fi
-fi
-
 export QWENPAW_PORT="${QWENPAW_PORT:-8088}"
 warn_if_auth_off_container_bind
 envsubst '${QWENPAW_PORT}' \

console/index.html
@@ -7,7 +7,7 @@
       name="viewport"
       content="width=device-width, initial-scale=1.0, viewport-fit=cover"
     />
-    <title>Nexora AI Platform</title>
+    <title>NexoraAIops 平台</title>
   </head>
   <body>
     <div id="root"></div>

console/public/logo-dark.svg
@@ -1,1 +1,1 @@
-<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="none" version="1.1" width="770.82177734375" height="132" viewBox="0 0 770.82177734375 132"><path d="M0,69.98228499999999C0,104.210785,27.535208,131.96455,61.494133,131.96455C69.605812,131.96455,77.3424,130.36953,84.434273,127.48661L74.130531,109.50395C70.145035,110.80365,65.901604,111.5125,61.494133,111.5125C38.776711,111.5125,20.290945,92.880051,20.290945,69.98228499999999C20.290945,47.084499,38.776711,28.452009,61.494133,28.452009C84.21154,28.452009,102.69729,47.084499,102.69729,69.98228499999999C102.69729,79.174469,99.719872,87.669563,94.679405,94.557785L90.764221,87.71682C90.248428,86.818863,89.287231,86.25174,88.255692,86.25174L78.010559,86.25174C76.979034,86.25174,76.017838,86.807045,75.502052,87.71682L72.899734,92.253815C72.383965,93.163589,72.383965,94.274223,72.899734,95.172188L76.33432,101.162422L79.780609,107.176353L89.955383,124.91094L94.022957,132L94.081589,132L116.09566,131.89367L105.452,113.30843C116.30666,102.13131,123,86.842484,123,69.970448L122.97652,69.98228499999999C122.97652,35.753796,95.453049,8,61.494133,8C27.535208,8,0,35.753796,0,69.98228499999999Z" fill="#000000" fill-opacity="1" style="mix-blend-mode:passthrough"/><path d="M143.881132,41.7247092L160.76963,98.113136L177.64629,41.7246926L197.527504,41.7246926L214.41592400000002,98.113136L231.31606,41.7246926L250.82172,41.7246926L224.12186,130.384132L204.20535999999998,130.384132L187.375641,74.208698L170.475433,130.384132L150.559053,130.384132L124,41.7246926L143.881132,41.7247092ZM329.72427,121.437729C339.72360000000003,113.248665,343.40886,102.136669,343.40886,102.136669L320.93404,102.136669L320.86354,102.219498C316.66202,107.355415,310.7587,110.92923,304.22165,111.816788C291.32356,113.568199,279.61067,105.722321,275.94903999999997,93.71094099999999L345.56827,93.651752C345.56827,93.651752,347.12926,86.799946,345.62694999999997,78.421562L345.40407,77.17900800000001C341.09685,54.753792000000004,321.15697,38.8253944,298.13050999999996,40.067954525C275.09222,41.310518,256.373,60.150054999999995,255.09367,83.39178100000001C253.56799,110.964752,276.28932,133.709457,303.69336999999996,131.898865C311.42758000000003,131.389984,320.82835,128.727386,329.72427,121.437729ZM276.32463,77.17900800000001C279.97449,66.95453499999999,289.75079,59.948881,300.64189,59.948881C311.53322000000003,59.948881,321.29781,66.954515,324.95947,77.17900800000001L276.33624,77.17900800000001L276.32463,77.17900800000001ZM443.95316,79.427437C443.87088,76.622814,443.61279,73.983875,443.19019,71.522413C439.55203,53.712402,423.88419,40.28097099,405.02402,40.0087919338C403.15808,40.0087919338,401.37411,40.11529851,399.66058,40.29280615C392.71277,41.1330104,386.32834,43.7482977,380.97653,47.6889749C380.29584,48.2806702,379.66214,48.907851199999996,379.04005,49.558726300000004L379.04005,41.7247092L359.54613,41.7247092L359.54613,130.384132L379.14569,130.384132C379.14569,130.384132,379.16909999999996,82.551598,379.16909999999996,82.44509500000001C379.19258,70.04316299999999,389.46179,60.079066999999995,401.83182,60.079066999999995C414.20181,60.079066999999995,424.22455,70.055012,424.49451,82.44509500000001L424.51791,82.44509500000001L424.51791,130.384132L444,130.384132L444,81.40369799999999C444,80.729179,443.97656,80.078312,443.95316,79.427437Z" fill="#000000" fill-opacity="1" style="mix-blend-mode:passthrough"/><path d="M641,41.724609375L667.559053,130.38404837500002L687.475433,130.38404837500002L704.375641,74.208614375L721.20536,130.38404837500002L741.12186,130.38404837500002L767.82172,41.724609375L748.31606,41.724609375L731.415924,98.113052375L714.527504,41.724609375L694.64629,41.724609375L677.76963,98.113052375L660.881132,41.724625945091L641,41.724609375Z" fill="#000000" fill-opacity="1" style="mix-blend-mode:passthrough"/><path d="M587,132C612.405098,132,633,112.076668,633,87.5C633,62.923328,612.405098,43,587,43C561.594902,43,541,62.923328,541,87.5C541,112.076668,561.594902,132,587,132ZM587.00021,112.860184C601.478386,112.860184,613.215263,101.506027,613.215263,87.499966C613.215263,73.493906,601.478386,62.139751000000004,587.00021,62.139751000000004C572.522034,62.139751000000004,560.785156,73.493906,560.785156,87.499966C560.785156,101.506027,572.522034,112.860184,587.00021,112.860184Z" fill-rule="evenodd" fill="#000000" fill-opacity="1"/><path d="M478,7L458,7L458,131L478,131L478,84L500.5,84C521.762962,84,541,66.762962,541,45.5C541,24.237036,521.762962,7,500.5,7L478,7ZM478,27L478,64L500.5,64C511.26955399999997,64,522,56.269554,522,45.5C522,34.730446,511.26955399999997,27,500.5,27L478,27Z" fill-rule="evenodd" fill="#000000" fill-opacity="1"/><path d="M586.5629964,31.053010546875C587.6683826,32.129556546875,588.9830451,32.975797546875,590.5069752,33.591731546874996C592.0309057,34.207666546875004,593.683621,34.518310546875,595.46512,34.518310546875C597.246618,34.518310546875,598.942264,34.202308546875,600.460827,33.575664546875004C601.984756,32.943662546875004,603.304787,32.086711546875,604.42627,30.994098546875C605.547752,29.901483546875,606.422407,28.632125546875,607.05559,27.186019546875C607.688774,25.739912546875,608,24.181331546875,608,22.520989546875L608,22.435294546875C608,20.774949546875,607.683411,19.216369646875002,607.05559,17.770263646875C606.422407,16.318801846875,605.55312,15.060153946875001,604.447735,13.983608446875C603.342348,12.907063246875,602.027685,12.060823446875,600.503756,11.444889486875C598.979825,10.828955526875,597.327108,10.518310546875,595.54561,10.518310546875C593.764112,10.518310546875,592.0684671,10.834311456875,590.5445366,11.460957346875C589.0206065,12.092959146875,587.7005768,12.949910646875,586.5790935,14.042524146875C585.4576101,15.135137546875,584.5829562,16.399140846875,583.94977289,17.850603146875C583.31658959,19.296709046875,583,20.855290546875,583,22.515633546875L583,22.601328546875C583,24.267028546875,583.31658959,25.820253546875,583.94977289,27.271715546875C584.5829562,28.717822546875,585.4522431,29.981826546875,586.5576291,31.053016546875L586.5629964,31.053010546875Z" fill="#FF9D4D" fill-opacity="1" style="mix-blend-mode:passthrough" transform="matrix(0.9900480508804321,-0.14072978496551514,0.14072978496551514,0.9900480508804321,4.32174675524584,82.15014232628164)"/><path d="M553.5622303,37.053012546874996C554.6673808,38.129558546875,555.9817591,38.975799546874995,557.5053644,39.591731546874996C559.0289698,40.207666546875004,560.68133,40.518310546875,562.462446,40.518310546875C564.243563,40.518310546875,565.938842,40.202308546875,567.457081,39.570306546875C568.980686,38.938304546875,570.300428,38.081354546875005,571.421673,36.988740546875C572.542919,35.896127546875,573.417383,34.632123546875,574.05043,33.180662546875C574.683477,31.734555546875,575.000002,30.175974546875,575.000002,28.515631546875L575.000002,28.429937546875C575.000002,26.769594546875,574.683477,25.216368646875,574.05043,23.764906846875C573.417383,22.313445546875002,572.548285,21.054797646875002,571.443132,19.983608246875C570.337982,18.907062746875,569.023603,18.060822946875,567.5,17.444889246875C565.976396,16.828955466875,564.324036,16.518310546875,562.542919,16.518310546875C560.761802,16.518310546875,559.0665245,16.834311546875,557.5429187,17.466313536875C556.0193133,18.098315446875,554.6995702,18.955267446875,553.5783262,20.047880146875C552.4570813,21.140493846875,551.5826178,22.409852946875,550.94957089,23.855959446874998C550.31652346,25.302065846875,550,26.860646546875,550,28.520989546875L550,28.606683546875C550,30.272384546875,550.31652346,31.825607546875,550.94957089,33.277070546874995C551.5826182,34.723176546874996,552.4517169,35.987180546874995,553.5568676,37.063726546875L553.5622303,37.053012546874996Z" fill="#FF9D4D" fill-opacity="1" style="mix-blend-mode:passthrough" transform="matrix(0.9900480508804321,-0.14072978496551514,0.14072978496551514,0.9900480508804321,3.1489537245070096,77.56577111713705)"/><path d="M614.5583899,49.433383640624996C615.6640134,50.509929640625,616.9789543,51.356170640624995,618.5032206,51.972102640624996C620.0274782,52.588037640625004,621.68055,52.898681640625,623.462435,52.898681640625C625.24432,52.898681640625,626.940322,52.582679640625,628.459215,51.950677640625C629.983473,51.318675640625,631.303787,50.461725640625005,632.42551,49.369111640625C633.547235,48.276498640625,634.422077,47.012494640625,635.055393,45.561033640625C635.688707,44.114926640625,636.000002,42.556345640625,636.000002,40.896002640625L636.000002,40.810308640624996C636.000002,39.149965640625,635.683344,37.596739740625,635.055393,36.145277940625C634.422077,34.693816640625,633.552599,33.435168740625,632.446976,32.363979340625C631.341352,31.287433840625,630.026411,30.441194040625,628.502146,29.825260340625C626.977887,29.209326560625,625.324816,28.898681640625,623.548303,28.898681640625C621.77179,28.898681640625,620.0704165,29.214682640625,618.5461583,29.846684630625C617.0219007,30.478686540625,615.7015872,31.335638540625,614.5798628,32.428251240625C613.4581385,33.520864940625,612.5832964,34.790224040625,611.94997704,36.236330540625C611.31665766,37.682436940624996,611,39.241017640625,611,40.901360640625L611,40.987054640625004C611,42.652755640625,611.31665766,44.205978640625,611.94997704,45.657441640624995C612.5832964,47.103547640624996,613.45277,48.367551640624995,614.558394,49.444097640625L614.5583899,49.433383640624996Z" fill="#FF9D4D" fill-opacity="1" style="mix-blend-mode:passthrough" transform="matrix(0.8999722599983215,-0.4359472095966339,0.4359472095966339,0.8999722599983215,48.5186495187736,269.25441487708304)"/><rect x="614" y="89" width="19" height="43" rx="0" fill="#000000" fill-opacity="1"/></svg>
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="none" version="1.1" width="770.82177734375" height="132" viewBox="0 0 770.82177734375 132"><path d="M0,69.98228499999999C0,104.210785,27.535208,131.96455,61.494133,131.96455C69.605812,131.96455,77.3424,130.36953,84.434273,127.48661L74.130531,109.50395C70.145035,110.80365,65.901604,111.5125,61.494133,111.5125C38.776711,111.5125,20.290945,92.880051,20.290945,69.98228499999999C20.290945,47.084499,38.776711,28.452009,61.494133,28.452009C84.21154,28.452009,102.69729,47.084499,102.69729,69.98228499999999C102.69729,79.174469,99.719872,87.669563,94.679405,94.557785L90.764221,87.71682C90.248428,86.818863,89.287231,86.25174,88.255692,86.25174L78.010559,86.25174C76.979034,86.25174,76.017838,86.807045,75.502052,87.71682L72.899734,92.253815C72.383965,93.163589,72.383965,94.274223,72.899734,95.172188L76.33432,101.162422L79.780609,107.176353L89.955383,124.91094L94.022957,132L94.081589,132L116.09566,131.89367L105.452,113.30843C116.30666,102.13131,123,86.842484,123,69.970448L122.97652,69.98228499999999C122.97652,35.753796,95.453049,8,61.494133,8C27.535208,8,0,35.753796,0,69.98228499999999Z" fill="#FFFFFF" fill-opacity="1" style="mix-blend-mode:passthrough"/><path d="M143.881132,41.7247092L160.76963,98.113136L177.64629,41.7246926L197.527504,41.7246926L214.41592400000002,98.113136L231.31606,41.7246926L250.82172,41.7246926L224.12186,130.384132L204.20535999999998,130.384132L187.375641,74.208698L170.475433,130.384132L150.559053,130.384132L124,41.7246926L143.881132,41.7247092ZM329.72427,121.437729C339.72360000000003,113.248665,343.40886,102.136669,343.40886,102.136669L320.93404,102.136669L320.86354,102.219498C316.66202,107.355415,310.7587,110.92923,304.22165,111.816788C291.32356,113.568199,279.61067,105.722321,275.94903999999997,93.71094099999999L345.56827,93.651752C345.56827,93.651752,347.12926,86.799946,345.62694999999997,78.421562L345.40407,77.17900800000001C341.09685,54.753792000000004,321.15697,38.8253944,298.13050999999996,40.067954525C275.09222,41.310518,256.373,60.150054999999995,255.09367,83.39178100000001C253.56799,110.964752,276.28932,133.709457,303.69336999999996,131.898865C311.42758000000003,131.389984,320.82835,128.727386,329.72427,121.437729ZM276.32463,77.17900800000001C279.97449,66.95453499999999,289.75079,59.948881,300.64189,59.948881C311.53322000000003,59.948881,321.29781,66.954515,324.95947,77.17900800000001L276.33624,77.17900800000001L276.32463,77.17900800000001ZM443.95316,79.427437C443.87088,76.622814,443.61279,73.983875,443.19019,71.522413C439.55203,53.712402,423.88419,40.28097099,405.02402,40.0087919338C403.15808,40.0087919338,401.37411,40.11529851,399.66058,40.29280615C392.71277,41.1330104,386.32834,43.7482977,380.97653,47.6889749C380.29584,48.2806702,379.66214,48.907851199999996,379.04005,49.558726300000004L379.04005,41.7247092L359.54613,41.7247092L359.54613,130.384132L379.14569,130.384132C379.14569,130.384132,379.16909999999996,82.551598,379.16909999999996,82.44509500000001C379.19258,70.04316299999999,389.46179,60.079066999999995,401.83182,60.079066999999995C414.20181,60.079066999999995,424.22455,70.055012,424.49451,82.44509500000001L424.51791,82.44509500000001L424.51791,130.384132L444,130.384132L444,81.40369799999999C444,80.729179,443.97656,80.078312,443.95316,79.427437Z" fill="#FFFFFF" fill-opacity="1" style="mix-blend-mode:passthrough"/><path d="M641,41.724609375L667.559053,130.38404837500002L687.475433,130.38404837500002L704.375641,74.208614375L721.20536,130.38404837500002L741.12186,130.38404837500002L767.82172,41.724609375L748.31606,41.724609375L731.415924,98.113052375L714.527504,41.724609375L694.64629,41.724609375L677.76963,98.113052375L660.881132,41.724625945091L641,41.724609375Z" fill="#FFFFFF" fill-opacity="1" style="mix-blend-mode:passthrough"/><path d="M587,132C612.405098,132,633,112.076668,633,87.5C633,62.923328,612.405098,43,587,43C561.594902,43,541,62.923328,541,87.5C541,112.076668,561.594902,132,587,132ZM587.00021,112.860184C601.478386,112.860184,613.215263,101.506027,613.215263,87.499966C613.215263,73.493906,601.478386,62.139751000000004,587.00021,62.139751000000004C572.522034,62.139751000000004,560.785156,73.493906,560.785156,87.499966C560.785156,101.506027,572.522034,112.860184,587.00021,112.860184Z" fill-rule="evenodd" fill="#FFFFFF" fill-opacity="1"/><path d="M478,7L458,7L458,131L478,131L478,84L500.5,84C521.762962,84,541,66.762962,541,45.5C541,24.237036,521.762962,7,500.5,7L478,7ZM478,27L478,64L500.5,64C511.26955399999997,64,522,56.269554,522,45.5C522,34.730446,511.26955399999997,27,500.5,27L478,27Z" fill-rule="evenodd" fill="#FFFFFF" fill-opacity="1"/><path d="M586.5629964,31.053010546875C587.6683826,32.129556546875,588.9830451,32.975797546875,590.5069752,33.591731546874996C592.0309057,34.207666546875004,593.683621,34.518310546875,595.46512,34.518310546875C597.246618,34.518310546875,598.942264,34.202308546875,600.460827,33.575664546875004C601.984756,32.943662546875004,603.304787,32.086711546875,604.42627,30.994098546875C605.547752,29.901483546875,606.422407,28.632125546875,607.05559,27.186019546875C607.688774,25.739912546875,608,24.181331546875,608,22.520989546875L608,22.435294546875C608,20.774949546875,607.683411,19.216369646875002,607.05559,17.770263646875C606.422407,16.318801846875,605.55312,15.060153946875001,604.447735,13.983608446875C603.342348,12.907063246875,602.027685,12.060823446875,600.503756,11.444889486875C598.979825,10.828955526875,597.327108,10.518310546875,595.54561,10.518310546875C593.764112,10.518310546875,592.0684671,10.834311456875,590.5445366,11.460957346875C589.0206065,12.092959146875,587.7005768,12.949910646875,586.5790935,14.042524146875C585.4576101,15.135137546875,584.5829562,16.399140846875,583.94977289,17.850603146875C583.31658959,19.296709046875,583,20.855290546875,583,22.515633546875L583,22.601328546875C583,24.267028546875,583.31658959,25.820253546875,583.94977289,27.271715546875C584.5829562,28.717822546875,585.4522431,29.981826546875,586.5576291,31.053016546875L586.5629964,31.053010546875Z" fill="#FFFFFF" fill-opacity="1" style="mix-blend-mode:passthrough" transform="matrix(0.9900480508804321,-0.14072978496551514,0.14072978496551514,0.9900480508804321,4.32174675524584,82.15014232628164)"/><path d="M553.5622303,37.053012546874996C554.6673808,38.129558546875,555.9817591,38.975799546874995,557.5053644,39.591731546874996C559.0289698,40.207666546875004,560.68133,40.518310546875,562.462446,40.518310546875C564.243563,40.518310546875,565.938842,40.202308546875,567.457081,39.570306546875C568.980686,38.938304546875,570.300428,38.081354546875005,571.421673,36.988740546875C572.542919,35.896127546875,573.417383,34.632123546875,574.05043,33.180662546875C574.683477,31.734555546875,575.000002,30.175974546875,575.000002,28.515631546875L575.000002,28.429937546875C575.000002,26.769594546875,574.683477,25.216368646875,574.05043,23.764906846875C573.417383,22.313445546875002,572.548285,21.054797646875002,571.443132,19.983608246875C570.337982,18.907062746875,569.023603,18.060822946875,567.5,17.444889246875C565.976396,16.828955466875,564.324036,16.518310546875,562.542919,16.518310546875C560.761802,16.518310546875,559.0665245,16.834311546875,557.5429187,17.466313536875C556.0193133,18.098315446875,554.6995702,18.955267446875,553.5783262,20.047880146875C552.4570813,21.140493846875,551.5826178,22.409852946875,550.94957089,23.855959446874998C550.31652346,25.302065846875,550,26.860646546875,550,28.520989546875L550,28.606683546875C550,30.272384546875,550.31652346,31.825607546875,550.94957089,33.277070546874995C551.5826182,34.723176546874996,552.4517169,35.987180546874995,553.5568676,37.063726546875L553.5622303,37.053012546874996Z" fill="#FFFFFF" fill-opacity="1" style="mix-blend-mode:passthrough" transform="matrix(0.9900480508804321,-0.14072978496551514,0.14072978496551514,0.9900480508804321,3.1489537245070096,77.56577111713705)"/><path d="M614.5583899,49.433383640624996C615.6640134,50.509929640625,616.9789543,51.356170640624995,618.5032206,51.972102640624996C620.0274782,52.588037640625004,621.68055,52.898681640625,623.462435,52.898681640625C625.24432,52.898681640625,626.940322,52.582679640625,628.459215,51.950677640625C629.983473,51.318675640625,631.303787,50.461725640625005,632.42551,49.369111640625C633.547235,48.276498640625,634.422077,47.012494640625,635.055393,45.561033640625C635.688707,44.114926640625,636.000002,42.556345640625,636.000002,40.896002640625L636.000002,40.810308640624996C636.000002,39.149965640625,635.683344,37.596739740625,635.055393,36.145277940625C634.422077,34.693816640625,633.552599,33.435168740625,632.446976,32.363979340625C631.341352,31.287433840625,630.026411,30.441194040625,628.502146,29.825260340625C626.977887,29.209326560625,625.324816,28.898681640625,623.548303,28.898681640625C621.77179,28.898681640625,620.0704165,29.214682640625,618.5461583,29.846684630625C617.0219007,30.478686540625,615.7015872,31.335638540625,614.5798628,32.428251240625C613.4581385,33.520864940625,612.5832964,34.790224040625,611.94997704,36.236330540625C611.31665766,37.682436940624996,611,39.241017640625,611,40.901360640625L611,40.987054640625004C611,42.652755640625,611.31665766,44.205978640625,611.94997704,45.657441640624995C612.5832964,47.103547640624996,613.45277,48.367551640624995,614.558394,49.444097640625L614.5583899,49.433383640624996Z" fill="#FFFFFF" fill-opacity="1" style="mix-blend-mode:passthrough" transform="matrix(0.8999722599983215,-0.4359472095966339,0.4359472095966339,0.8999722599983215,48.5186495187736,269.25441487708304)"/><rect x="614" y="89" width="19" height="43" rx="0" fill="#FFFFFF" fill-opacity="1"/></svg>

console/public/logo-icon.svg
@@ -1,1 +1,18 @@
-<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="none" version="1.1" width="175.35000610351562" height="235.4199981689453" viewBox="0 0 175.35000610351562 235.4199981689453"><defs><clipPath id="master_svg0_145_28391"><rect x="0" y="0" width="175.35000610351562" height="235.4199981689453" rx="0"/></clipPath></defs><g clip-path="url(#master_svg0_145_28391)"><path d="M83.10836,229.9998485546875C129.00784,229.9998485546875,166.21672,193.9784385546875,166.21672,149.5438075546875C166.21672,105.1091615546875,129.00784,69.0877685546875,83.10836,69.0877685546875C37.208885,69.0877685546875,0,105.1091615546875,0,149.5438075546875C0,193.9784385546875,37.208885,229.9998485546875,83.10836,229.9998485546875ZM83.108734,195.39502855468749C109.26651,195.39502855468749,130.47157,174.8667085546875,130.47157,149.5437395546875C130.47157,124.2207715546875,109.26651,103.6924515546875,83.108734,103.6924515546875C56.950966,103.6924515546875,35.745914,124.2207715546875,35.745914,149.5437395546875C35.745914,174.8667085546875,56.950966,195.39502855468749,83.108734,195.39502855468749Z" fill-rule="evenodd" fill="#000000" fill-opacity="1"/><path d="M80.31835555625,46.48915884375C82.31537815625,48.43563484375,84.69048165625,49.96570184375,87.44365765625,51.07936084375C90.19683265625,52.19301584375,93.18267265624999,52.75468784375,96.40117865625,52.75468784375C99.61968465625,52.75468784375,102.68308465625,52.18332684375,105.42656165625,51.05030784375C108.17973665625,49.90760084375,110.56454065625,48.35816584375,112.59064065625,46.38263684375C114.61674165625,44.40710784375,116.19691465625,42.11201084375,117.34083965625,39.49733884375C118.48477165624999,36.882665843750004,119.04704265625,34.064632843750005,119.04704265625,31.06260484375L119.04704265625,30.90766184375C119.04704265625,27.90563184375,118.47508265625001,25.08759984375,117.34083965625,22.47292784375C116.19691465625,19.84857384375,114.62644165625,17.57284164375,112.62942165625,15.626364743749999C110.63239665625,13.679887743750001,108.25728965625001,12.14982174375,105.50411765625,11.03616584375C102.75094265625,9.92250990375,99.76509865625,9.36083984375,96.54659465625,9.36083984375C93.32809265624999,9.36083984375,90.26469065625,9.93219375375,87.51151765624999,11.06521774375C84.75834165625,12.207925543750001,82.37353995625,13.757359543749999,80.34743685625,15.73288914375C78.32133435625,17.70841784375,76.74116035625,19.99383284375,75.59723355625,22.618188843749998C74.45330721625,25.23285984375,73.88134765625,28.05089384375,73.88134765625,31.05292284375L73.88134765625,31.20786484375C73.88134765625,34.21957984375,74.45330721625,37.02792784375,75.59723355625,39.65228284375C76.74116035625,42.26695984375,78.31163835625,44.55237584375,80.30865955625,46.48916284375L80.31835555625,46.48915884375Z" fill="#FF9D4D" fill-opacity="1" style="mix-blend-mode:passthrough" transform="matrix(0.9901291728019714,-0.14083673059940338,0.14062289893627167,0.9899662137031555,-0.5870784194339649,10.499132122735318)"/><path d="M21.69596002224121,58.3371813046875C23.692555422241213,60.2836573046875,26.06714692224121,61.8137283046875,28.81973592224121,62.9273833046875C31.57232092224121,64.0410383046875,34.55752192224121,64.60271030468749,37.77533692224121,64.60271030468749C40.99315292224121,64.60271030468749,44.05588892224121,64.0313493046875,46.79878192224121,62.8886373046875C49.55137092224121,61.7459373046875,51.93565192224121,60.1965023046875,53.96132492224121,58.2209703046875C55.98699792224121,56.2454453046875,57.566831922241214,53.9600293046875,58.71051192224121,51.3356743046875C59.854188922241214,48.7210023046875,60.42603492224121,45.9029693046875,60.42603492224121,42.900940304687495L60.42603492224121,42.745998304687504C60.42603492224121,39.7439723046875,59.854188922241214,36.935620304687504,58.71051192224121,34.3112663046875C57.566831922241214,31.6869113046875,55.996690922241214,29.4111795046875,54.00008992224121,27.4743867046875C52.00349992224121,25.5279102046875,49.628903922241214,23.9978435046875,46.87632192224121,22.8841878046875C44.12373592224121,21.7705321946875,41.138536922241215,21.2088623046875,37.920720922241216,21.2088623046875C34.702905922241214,21.2088623046875,31.640168922241212,21.7802163346875,28.88758192224121,22.9229246046875C26.134994922241212,24.0656326046875,23.75070952224121,25.6150675046875,21.725039022241212,27.5905948046875C19.69936752224121,29.5661259046875,18.11953732224121,31.8612243046875,16.97585702224121,34.4758963046875C15.832175972241211,37.0905673046875,15.260335922241211,39.9085983046875,15.260335922241211,42.9106273046875L15.260335922241211,43.065571304687495C15.260335922241211,46.0772853046875,15.832175972241211,48.8856313046875,16.97585702224121,51.5099883046875C18.11953782224121,54.1246603046875,19.689675822241213,56.4100763046875,21.68627162224121,58.3565523046875L21.69596002224121,58.3371813046875Z" fill="#FF9D4D" fill-opacity="1" style="mix-blend-mode:passthrough" transform="matrix(0.9901291728019714,-0.14083673059940338,0.14062289893627167,0.9899662137031555,-2.8318195615530612,2.3620210111015183)"/><path d="M128.877099025,80.714233296875C130.874905625,82.660366296875,133.250938625,84.190162296875,136.005211625,85.303615296875C138.759467625,86.417068296875,141.746486625,86.97863729687501,144.966264625,86.97863729687501C148.186042625,86.97863729687501,151.250633625,86.407386296875,153.995200625,85.26488529687501C156.749454625,84.12237929687501,159.135200625,82.573223296875,161.162097625,80.59804129687501C163.188995625,78.62287129687499,164.769794625,76.337856296875,165.914161625,73.713974296875C167.058528625,71.099767296875,167.621028625,68.282234296875,167.621028625,65.280740296875L167.621028625,65.125827296875C167.621028625,62.124336296875,167.048839625,59.316484296875004,165.914161625,56.692597296875C164.769794625,54.068712296875,163.198684625,51.793384596875,161.200881625,49.856937396875C159.203075625,47.910807096875,156.827041625,46.381013396875,154.072772625,45.267556196875C151.318509625,44.154099106875,148.331493625,43.592529296875,145.121423625,43.592529296875C141.911352625,43.592529296875,138.837056625,44.163781586875,136.082798625,45.306286096875C133.328540625,46.448790596875,130.942799525,47.997950096875,128.915899725,49.973125496875C126.888999425,51.948304196875,125.308203425,54.242992296875,124.163826825,56.857199296875C123.019450185,59.471404296875,122.447265625,62.288936296875,122.447265625,65.290430296875L122.447265625,65.445343296875C122.447265625,68.456518296875,123.019450185,71.264366296875,124.163826825,73.888256296875C125.308203425,76.502460296875,126.879299125,78.787471296875,128.877106225,80.733600296875L128.877099025,80.714233296875Z" fill="#FF9D4D" fill-opacity="1" style="mix-blend-mode:passthrough" transform="matrix(0.8998494148254395,-0.43620070815086365,0.43569377064704895,0.9000949859619141,-6.729828156028816,57.76669622812187)"/><rect x="131.88916015625" y="152.255859375" width="34.327369689941406" height="77.7440414428711" rx="0" fill="#000000" fill-opacity="1"/></g></svg>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" role="img" aria-label="创金AIops 平台">
+  <defs>
+    <linearGradient id="gold" x1="12" y1="8" x2="52" y2="56" gradientUnits="userSpaceOnUse">
+      <stop offset="0" stop-color="#ffd11a"/>
+      <stop offset="0.48" stop-color="#f5a900"/>
+      <stop offset="1" stop-color="#e88a00"/>
+    </linearGradient>
+    <linearGradient id="gold-dark" x1="18" y1="10" x2="44" y2="50" gradientUnits="userSpaceOnUse">
+      <stop offset="0" stop-color="#f9b600"/>
+      <stop offset="1" stop-color="#d87800"/>
+    </linearGradient>
+  </defs>
+  <path d="M32 4 56 18v28L32 60 8 46V18L32 4Z" fill="url(#gold)"/>
+  <path d="M32 11 50 21.5v21L32 53 14 42.5v-21L32 11Z" fill="none" stroke="#fff" stroke-width="5" stroke-linejoin="round" opacity=".96"/>
+  <path d="M24.5 40.5 41 24" fill="none" stroke="#fff" stroke-width="6" stroke-linecap="round"/>
+  <path d="M21 34.5 34.5 21" fill="none" stroke="url(#gold-dark)" stroke-width="5.5" stroke-linecap="round"/>
+  <path d="M29.5 43 43 29.5" fill="none" stroke="url(#gold-dark)" stroke-width="5.5" stroke-linecap="round"/>
+</svg>

console/src/api/modules/tokenUsage.ts
@@ -31,7 +31,7 @@ export const tokenUsageApi = {
   getTokenUsageDetails: (params: GetTokenUsageParams) =>
     request<TokenUsageRecord[]>(`/token-usage/details${buildQuery(params)}`),
 
-  // Per-user token usage (CJ AIops PG)
+  // Per-user token usage (Nexora PG)
   getTokenUsageByUser: (params: { start_date: string; end_date: string }) => {
     const search = new URLSearchParams({
       start_date: params.start_date,

commit 4cd48c21bb39649b77c94bf7c537885ec451e01d

fix: restore Alembic auto-migration block in entrypoint.sh
Commit 4a57717 accidentally removed the Alembic migration block during
the CJ_AIOPS→NEXORA env var sanitization sync. Restore it so container
startup runs `alembic upgrade head` automatically when NEXORA_DB_URL is set.

deploy/entrypoint.sh
@@ -43,6 +43,16 @@ else
   echo "Config found in ${QWENPAW_WORKING_DIR}, skipping initialization."
 fi
 
+# Run Alembic migrations if NEXORA_DB_URL is set.
+if [ -n "${NEXORA_DB_URL:-}" ]; then
+    echo "Running database migrations..."
+    if alembic upgrade head; then
+        echo "Database migrations complete."
+    else
+        echo "WARNING: Database migration failed. Tables may be created on first access." >&2
+    fi
+fi
+
 export QWENPAW_PORT="${QWENPAW_PORT:-8088}"
 warn_if_auth_off_container_bind
 envsubst '${QWENPAW_PORT}' \